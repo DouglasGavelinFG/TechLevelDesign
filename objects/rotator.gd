ffc8cfad6450171e6b5e1c228910203e080d6995
DouglasGavelinFG <douglas.gavelin@edu.futuregames.nu>
2024-11-25 14:12:28 +0100

    init testing

---
+++ objects/rotator.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var rotation_vector = 0;
+@export var rotation_speed = 0.01;
+
+var time = 0.0
+
+var random_number = RandomNumberGenerator.new()
+
+var random_velocity:float
+var random_time:float
+
+func _ready():
+	
+	random_velocity = random_number.randf_range(0.1, 2.0)
+	random_time = random_number.randf_range(0.1, 2.0)
+
+func _process(delta):
+	
+	#rotation.z += (cos(time * random_time) * random_velocity) * delta # Sine movement
+	rotation[rotation_vector] += rotation_speed
+	
+	time += delta
